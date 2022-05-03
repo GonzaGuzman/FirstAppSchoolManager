2ed8307bb255ec1036bd1fb5eebe00d71662af56
GonzaGuzman <gonzaguzmanz@gmail.com>
2022-05-03 19:57:56 -0300

    "addRetroFit"

--- app/src/main/java/com/zalo/firstAppMVP/registration/registrationDataSource/RegistrationDataSource.kt
+++ app/src/main/java/com/zalo/firstAppMVP/registration/registrationDataSource/RegistrationDataSource.kt
@@ -2,7 +2,6 @@ package com.zalo.firstAppMVP.registration.registrationDataSource
 
 import com.zalo.firstAppMVP.network.models.Schools
 import com.zalo.firstAppMVP.registration.registrationRepository.RegistrationRepository
-import com.zalo.firstAppMVP.util.sharedPreferences.MySharedPreferences
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
